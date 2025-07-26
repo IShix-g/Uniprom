a7527c8e443ac69feca06b02931ce015e800a434
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-07-26 18:21:43 +0900

    update AddressableAssetSettings and editor utilities

--- Assets/Test/Editor/OpenSavePath.cs
+++ Assets/Test/Editor/OpenSavePath.cs
@@ -1,37 +1,48 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
 
-namespace harapeco.editor
+namespace Editor
 {
-	public class OpenSavePath : EditorWindow
-	{
-		[MenuItem("Tools/永続保存フォルダを開く")]
-		public static void Open()
-		{
-			if (Application.platform == RuntimePlatform.OSXEditor)
-			{
-				System.Diagnostics.Process.Start(Application.persistentDataPath);
-			}
-			else if (Application.platform == RuntimePlatform.WindowsEditor)
-			{
-				EditorUtility.RevealInFinder(Application.persistentDataPath);
-			}
-		}
+    public class OpenSavePath : EditorWindow
+    {
+        [MenuItem("Tools/open persistent")]
+        public static void Open()
+        {
+            if (Application.platform == RuntimePlatform.OSXEditor)
+            {
+                System.Diagnostics.Process.Start(Application.persistentDataPath);
+            }
+            else if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                EditorUtility.RevealInFinder(Application.persistentDataPath);
+            }
+        }
 
-        [MenuItem("Tools/一時保存フォルダを開く")]
-		public static void OpenTmp()
-		{
-			if (Application.platform == RuntimePlatform.OSXEditor)
-			{
-				System.Diagnostics.Process.Start(Application.temporaryCachePath);
-			}
-			else if (Application.platform == RuntimePlatform.WindowsEditor)
-			{
-				EditorUtility.RevealInFinder(Application.temporaryCachePath);
-			}
-		}
-	}
-
+        [MenuItem("Tools/open temporary cache")]
+        public static void OpenTmp()
+        {
+            if (Application.platform == RuntimePlatform.OSXEditor)
+            {
+                System.Diagnostics.Process.Start(Application.temporaryCachePath);
+            }
+            else if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                EditorUtility.RevealInFinder(Application.temporaryCachePath);
+            }
+        }
+		
+        [MenuItem("Tools/open default cache")]
+        public static void OpenCache()
+        {
+            var cachePath = Caching.defaultCache.path;
+            if (Application.platform == RuntimePlatform.OSXEditor)
+            {
+                System.Diagnostics.Process.Start(cachePath);
+            }
+            else if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                EditorUtility.RevealInFinder(cachePath);
+            }
+        }
+    }
 }
-
