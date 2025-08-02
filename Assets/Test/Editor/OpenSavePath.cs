2c803a0ddccbd17ace43cc3c09499eed6cb68cf3
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-08-02 23:40:09 +0900

    remove unnecessary namespace in OpenSavePath

--- Assets/Test/Editor/OpenSavePath.cs
+++ Assets/Test/Editor/OpenSavePath.cs
@@ -3,53 +3,50 @@ using System.IO;
 using UnityEngine;
 using UnityEditor;
 
-namespace Editor
+public class OpenSavePath : EditorWindow
 {
-    public class OpenSavePath : EditorWindow
+    [MenuItem("Tools/open path/persistent")]
+    public static void Open()
     {
-        [MenuItem("Tools/open path/persistent")]
-        public static void Open()
+        if (Application.platform == RuntimePlatform.OSXEditor)
         {
-            if (Application.platform == RuntimePlatform.OSXEditor)
-            {
-                System.Diagnostics.Process.Start(Application.persistentDataPath);
-            }
-            else if (Application.platform == RuntimePlatform.WindowsEditor)
-            {
-                EditorUtility.RevealInFinder(Application.persistentDataPath);
-            }
+            System.Diagnostics.Process.Start(Application.persistentDataPath);
         }
+        else if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            EditorUtility.RevealInFinder(Application.persistentDataPath);
+        }
+    }
 
-        [MenuItem("Tools/open path/temporary cache")]
-        public static void OpenTmp()
+    [MenuItem("Tools/open path/temporary cache")]
+    public static void OpenTmp()
+    {
+        if (Application.platform == RuntimePlatform.OSXEditor)
         {
-            if (Application.platform == RuntimePlatform.OSXEditor)
-            {
-                System.Diagnostics.Process.Start(Application.temporaryCachePath);
-            }
-            else if (Application.platform == RuntimePlatform.WindowsEditor)
-            {
-                EditorUtility.RevealInFinder(Application.temporaryCachePath);
-            }
+            System.Diagnostics.Process.Start(Application.temporaryCachePath);
         }
-		
-        [MenuItem("Tools/open path/default cache")]
-        public static void OpenCache()
+        else if (Application.platform == RuntimePlatform.WindowsEditor)
         {
-            var cachePath = Caching.defaultCache.path;
-            if (!Directory.Exists(cachePath))
-            {
-                Debug.LogWarning("cache path not found path: " + cachePath);
-                return;
-            }
-            if (Application.platform == RuntimePlatform.OSXEditor)
-            {
-                System.Diagnostics.Process.Start(cachePath);
-            }
-            else if (Application.platform == RuntimePlatform.WindowsEditor)
-            {
-                EditorUtility.RevealInFinder(cachePath);
-            }
+            EditorUtility.RevealInFinder(Application.temporaryCachePath);
+        }
+    }
+	
+    [MenuItem("Tools/open path/default cache")]
+    public static void OpenCache()
+    {
+        var cachePath = Caching.defaultCache.path;
+        if (!Directory.Exists(cachePath))
+        {
+            Debug.LogWarning("cache path not found path: " + cachePath);
+            return;
+        }
+        if (Application.platform == RuntimePlatform.OSXEditor)
+        {
+            System.Diagnostics.Process.Start(cachePath);
+        }
+        else if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            EditorUtility.RevealInFinder(cachePath);
         }
     }
 }
